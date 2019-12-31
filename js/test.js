4213e0213f8e622e17d473f8ef5c30ea982ed3a6
isakfk1234 <isak.boler@gmail.com>
2019-12-31 11:27:04 +0100

    commit

---
+++ js/test.js
@@ -0,0 +1,16 @@
+var math = getelementById()
+
+Object.isExtensible(Object)
+Object.isFrozen(Object)
+
+
+
+for (i < 1; 1; math[i]);
+
+var counter;
+var lowercounter;
+
+function counter() {
+    lowercounter.parent.parent; 
+    for (i > 1; i; counter[i]);
+}
