ef7d7fc2989ac7c996c6a208dc86b1aa0fac31f3
isakfk1234 <isak.boler@gmail.com>
2020-05-24 00:37:48 +0200

    commit

--- pages/login/js/login.js
+++ pages/login/js/login.js
@@ -12,19 +12,19 @@ if(a=="isakfk1234" && b=="12345")
 window.open("/nettsider/flightweek/making/account/account.html");
 }
 else
-if(a=="wild1234" && b=="12345")
+if(a=="Leon" && b=="12345")
 {
-window.open("/nettsider/flightweek/pages/accounts/usersprofil/wild1234.html");
+window.open("/nettsider/flightweek/making/account/accounts/leon/leon.html");
 }
 else
-if(a=="vikingboy12" && b=="12345")
+if(a=="David" && b=="12345")
 {
-window.open("tv2.html");
+window.open("/nettsider/flightweek/making/account/accounts/david/david.html");
 }
 else
-if(a=="AB12" && b=="12345")
+if(a=="Jodi12" && b=="12345")
 {
-window.open("manager.html");
+window.open("/nettsider/flightweek/making/account/accounts/Jodi12/Jodi12.html");
 }
 else
 if(a=="osi32" && b=="12345")
